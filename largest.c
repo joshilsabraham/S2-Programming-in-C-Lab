b472b1d7c0c46bf7bd2b1dc5fa35f1a50a9b0bca
Joshil S Abraham <80256698+mighty070jail@users.noreply.github.com>
2025-02-04 16:29:39 +0530

    Update largest.c

--- largest.c
+++ largest.c
@@ -1,6 +1,9 @@
+/*	Author: Joshil S Abraham
+	Date: 28/01/2025
+	Description: C Program to find the Largest of Three Numbers using Conditional Statements	*/
+
 #include <stdio.h>
-int main()
-	{
+int main(){
 	int num1,num2,num3;
 	printf("Enter three numbers:");
 	scanf("%d %d %d",&num1,&num2,&num3);

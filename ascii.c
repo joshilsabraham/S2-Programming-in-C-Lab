e0b3041b8d5ef6ac65fc332ec9f576528f6f9043
Joshil S Abraham <80256698+mighty070jail@users.noreply.github.com>
2025-02-04 16:26:09 +0530

    Update ascii.c

--- ascii.c
+++ ascii.c
@@ -1,10 +1,9 @@
-/* Author: Joshil S Abraham
-   Date: 28/01/2025
-   Description: C Program to find ASCII Value of a Character. */
+/*	Author: Joshil S Abraham
+   	Date: 28/01/2025
+   	Description: C Program to find ASCII Value of a Character. 	*/
 
 #include <stdio.h>
-int main()
-	{
+int main(){
 	char c;
 	printf("Enter value:") ;
 	scanf("%c",&c);

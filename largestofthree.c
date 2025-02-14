b5714bd7b348e80b40f10071b28c1b2a621259c8
Joshil S Abraham <80256698+mighty070jail@users.noreply.github.com>
2025-02-14 12:40:14 +0530

    Update largestofthree.c

--- largestofthree.c
+++ largestofthree.c
@@ -1,13 +1,19 @@
-/*	Author: Joshil S Abraham
-	Date: 28/01/2025
-	Description: C Program to find the Largest of Three Numbers using nested if statements. 	*/
+/*
+    Author: Joshil S Abraham
+    Date: 04/02/2025
+    Description: C Program to find the Largest of Three Numbers using nested if statements. 	
+*/
 
 #include <stdio.h>
-int main()
-{
+
+int main(){
 	int num1,num2,num3;
+
+	//Prompt user for input
 	printf("Enter three numbers:");
 	scanf("%d %d %d",&num1,&num2,&num3);
+
+	//Check for the largest number
 	if (num1>num2){
 		if (num1>num3){
 			printf("The Largest Among the Given Three is %d\n",num1);

e7c14e637dfd4c109cf400bf0af68d0b52987ba3
Joshil S Abraham <80256698+joshilsabraham@users.noreply.github.com>
2025-04-07 10:48:47 +0530

    Update ascii.c

--- ascii.c
+++ ascii.c
@@ -1,3 +1,9 @@
+/*	
+    Author: Joshil S Abraham
+    Date: 
+    Description: C Program to read a single character input from the user and then display the ASCII value associated with that character. 	
+*/
+
 #include <stdio.h>
 int main()
 	{
